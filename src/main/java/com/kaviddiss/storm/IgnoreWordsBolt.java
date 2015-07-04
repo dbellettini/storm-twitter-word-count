0ae7b4d9b9744aa2205d4fef81529513ef7de7c0
Davide Bellettini <davide@bellettini.me>
2015-07-04 12:53:28 +0200

    Ignore https word

--- src/main/java/com/kaviddiss/storm/IgnoreWordsBolt.java
+++ src/main/java/com/kaviddiss/storm/IgnoreWordsBolt.java
@@ -22,7 +22,7 @@ public class IgnoreWordsBolt extends BaseRichBolt {
     private Set<String> IGNORE_LIST = new HashSet<String>(Arrays.asList(new String[] {
             "http", "the", "you", "que", "and", "for", "that", "like", "have", "this", "just", "with", "all", "get", "about",
             "can", "was", "not", "your", "but", "are", "one", "what", "out", "when", "get", "lol", "now", "para", "por",
-            "want", "will", "know", "good", "from", "las", "don", "people", "got", "why", "con", "time", "would",
+            "want", "will", "know", "good", "from", "las", "don", "people", "got", "why", "con", "time", "would", "https"
     }));
     private OutputCollector collector;
 
